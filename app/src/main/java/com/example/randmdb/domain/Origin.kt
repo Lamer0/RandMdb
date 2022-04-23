22db38af9bdeb0b17dca726eb0ccb0cec0268075
Lamer0 <combinerk@gmail.com>
2022-04-23 18:23:50 +0300

    Rebase, architecture, write domain layer

--- app/src/main/java/com/example/randmdb/domain/Origin.kt
+++
@@ -1,6 +0,0 @@
-package com.example.randmdb.domain
-
-data class Origin(
-    val name: String,
-    val url: String
-)
--- app/src/main/java/com/example/randmdb/domain/queries/Characters.kt
+++
@@ -1,6 +0,0 @@
-package com.example.randmdb.domain.queries
-
-data class Characters(
-    val info: Info,
-    val results: List<Result>
-)
